7b5377e9fce5eb9e4435166cd47b3d76617945bb
chuangying2017 <631711487@qq.com>
2019-08-25 18:56:32 +0800

    添加爬虫路径

--- test.py
+++ test.py
@@ -29,15 +29,123 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import TimeoutException, NoSuchElementException, NoSuchAttributeException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, NoSuchAttributeException, StaleElementReferenceException
+import json
+from pyquery import PyQuery
+import time
+from multiprocessing import Pool
+import random
 
 
 
-url = 'https://www.xbiquge6.com'
+url = 'https://www.xbiquge6.com/'
 headers = {
     'Host': 'www.xbiquge6.com',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
 }
+
+
+def fetch_fiction(tup: tuple = (0, 2)):
+    global url, headers
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    browser = webdriver.Chrome(options=chrome_options)
+    new_url = url + str(tup[0]) + '_' + str(tup[1]) + '/'
+    try:
+        browser.get(new_url)
+        id_list = browser.find_element_by_id('list')
+        maininfo = browser.find_element_by_id('maininfo')
+        box_con = browser.find_elements(By.CSS_SELECTOR, '.box_con .con_top a')[1]
+        maininfo_p = maininfo.find_elements(By.TAG_NAME, 'p')
+        top_for = maininfo_p[:-2]
+        fiction = {
+            'class_name': box_con.text,  # 文章分类
+            'title': maininfo.find_element_by_tag_name('h1').text,  # 小说标题
+            'desc': maininfo_p[-2].text
+        }
+        ls_fiction_info = []
+        for p in top_for:
+            ls_fiction_info.append(p.text.split('：')[1])
+        fiction['author'] = ls_fiction_info[0]
+        fiction['status'] = ls_fiction_info[1]
+        fiction['last_update_time'] = ls_fiction_info[2]
+        fiction['latest_chapter'] = ls_fiction_info[3]
+        dt: list = []
+        dd = id_list.find_elements(By.TAG_NAME, 'dd')
+        content_script = "return document.getElementById('content').innerHTML"
+
+        storage_ls: list = []
+
+        for l in dd:
+            storage_ls.append((l.find_element(By.TAG_NAME, 'a').get_attribute('href'), l.text))
+
+        for k, d in enumerate(storage_ls):
+            print('制作文章中...' + d[1])
+            fiction_chapter: dict = {
+                'title': d[1]
+            }
+            browser.get(d[0])
+            wait = WebDriverWait(browser, 10)
+            wait.until(EC.presence_of_element_located((By.ID, 'content')))
+            content = browser.execute_script(content_script)
+            fiction_chapter['content'] = content
+            dt.append(fiction_chapter)
+            print('完成小部分.....' + str(k+1))
+        fiction['data'] = dt
+        path = os.getcwd() + '\\xiaoshuo'
+        os.chdir(path)
+        filename = str(fiction['title']) + '.txt'
+        f = open(filename, 'w+')
+        f.write(json.dumps(fiction))
+        f.close()
+        try:
+            batch_data(path+'\\'+filename)
+        except EnvironmentError:
+            print('环境出错')
+    except TimeoutException:
+        print('request page Time Out')
+    except NoSuchElementException:
+        print('网页元素没找到')
+    finally:
+        browser.close()
+    print('文章制作完成...')
+
+
+def func_forward(tup: tuple = (0, 3)):
+    new_num = 0
+    if tup[0] == 0:
+        new_num = 4
+    origin_vs = tup[0]
+    vs = 10 + origin_vs
+    filename = str(tup[0]) + '.txt'
+    back_tup = tup
+    if os.path.exists(filename):
+        open_file = open(filename, 'r+')
+        read_line = open_file.readline()
+        tup = tuple(eval(read_line))
+        if tup.__len__() < 1:
+            tup = back_tup
+    else:
+        open_file = open(filename, 'w')
+
+    for i in range(origin_vs, vs):
+        for j in range(new_num, tup[1]):
+            tup1 = (i, j + 1)
+            print(tup1)
+            try:
+                fetch_fiction(tup1)
+            except Exception:
+                print('数据库插入异常')
+            finally:
+                print('继续执行....' + str(j))
+            open_file.write(','.join('%s' %id for id in tup1))
+            open_file.close()
+            open_file = open(filename, 'r+')
+            time.sleep(random.uniform(0.5, 1.8))
+        filename = str(i) + '.txt'
+        open(filename, 'w')
+    open_file.close()
 
 
 def t_request():
@@ -65,7 +173,7 @@ def t_request():
             requests.post('http://192.168.1.9:8093/polls/post_fiction_chapter/', data={
                 'fiction_id': 1,
                 'title': dd[1],
-                'content': content
+                'content': content.text
             })
         print('执行完成....!')
     except NoSuchElementException:
@@ -74,5 +182,57 @@ def t_request():
         browser.close()
 
 
+def batch_data(filename: str):
+
+    try:
+
+        # if ~data.__len__():
+        #     dl = []
+        #     for i in range(100):
+        #         dl.append({'status': 'success', 'msg': 'operation mode ok'})
+        #     data['data'] = dl
+        file = open(filename, 'r')
+        ls_ = file.read()
+        data = json.loads(ls_)
+
+        res = requests.post('http://192.168.1.9:8093/polls/post_batch_create_data/', json=data, headers={
+            'Host': '192.168.1.9:8093',
+            'Accept': 'application/json'
+        })
+    except InterruptedError:
+        print('500内部出错')
+    else:
+        print(res.json())
+
+
+def xiaoshuo():
+    path = os.getcwd() + '\\xiaoshuo'
+    os.mkdir(path)
+    os.chdir(path)
+    file = open('dd.txt', 'w')
+    file.write('111')
+    file.close()
+
+    file1 = open(path + '\\dd.txt', 'r')
+    print(file1.read())
+    file1.close()
+    print('success')
+
+
 if __name__ == '__main__':
-    t_request()
+    xiaoshuo();exit()
+    ls: list = []
+    suffix = 99
+    min_num = 99999
+    v = 10
+    while suffix > 9:
+        suffix = suffix - v
+        ls.append((suffix, min_num))
+        if suffix <= 9:
+            ls.append((0, min_num))
+    ls.sort()
+    pool = Pool(processes=10)
+    pool.map(func_forward, ls)
+    pool.close()
+    pool.join()
+
